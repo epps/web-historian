54574906737ae2cbc4da06178976a13768fb5895
MKS-students <mks.students@hackreactor.com>
2015-07-22 21:44:16 -0700

    implements helper functions through isUrlArchived

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
+var url = require('url');
+var request = require('request');
 
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
@@ -25,17 +27,52 @@ exports.initialize = function(pathsObj){
 // The following function names are provided to you to suggest how you might
 // modularize your code. Keep it clean!
 
-exports.readListOfUrls = function(){
+exports.readListOfUrls = function(callback){
+  fs.readFile(exports.paths.list, function(error, data) {
+    var dataStr = data.toString().split('\n');
+    callback(dataStr);
+  });
 };
 
-exports.isUrlInList = function(){
+exports.isUrlInList = function(target,callback){
+  exports.readListOfUrls(function(dataStr) {
+    var targetIsFound = _.contains(dataStr, target);
+    callback(targetIsFound);
+  });
 };
 
-exports.addUrlToList = function(){
+exports.addUrlToList = function(uri,callback){
+  exports.isUrlInList(uri, function(uriFound) {
+    if(!uriFound) {
+      fs.appendFile(exports.paths.list, uri, function() {
+      callback();
+     });
+    }
+  });
 };
 
-exports.isUrlArchived = function(){
+exports.isUrlArchived = function(uri, callback){
+  exports.isUrlInList(uri, function(found) {
+    var uriPath = path.join(exports.paths.archivedSites, url.parse("http://" + uri).hostname + ".html");
+    fs.exists(uriPath, function() {
+      callback();
+    });
+  });
 };
 
-exports.downloadUrls = function(){
+exports.downloadUrls = function(uri, callback){
+
+      request(uri).pipe(fs.createWriteStream(exports.paths.archivedSites));
+    }
+
+
+  // what if there are multiple urls? can we split on /n? is it an array?
+  // can we use a length property?
+  request(uri).pipe(fs.createWriteStream(exports.paths.archivedSites));
 };
+
+
+
+
+
+
